a482240337bd05fc20446b7452c95d2ff93618c1
Caleb Carithers <calebcarithers@me.com>
2023-07-08 15:56:54 -0400

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,21 +1,4 @@
 import { Endpoints } from "@octokit/types";
-import { Case, CaseSource } from "./interfaces";
-
-export async function getCaseSource(slug: string): Promise<CaseSource> {
-  const res = await fetch("http://localhost:3000/api/case/" + slug);
-  if (!res.ok) {
-    throw new Error("Failed to fetch case");
-  }
-  return res.json();
-}
-
-export async function getCases(): Promise<Array<Case>> {
-  const res = await fetch("http://localhost:3000/api/case");
-  if (!res.ok) {
-    throw new Error("Failed to fetch cases");
-  }
-  return res.json();
-}
 
 export type GithubPullResponse =
   Endpoints["GET /repos/{owner}/{repo}/pulls"]["response"]["data"];
