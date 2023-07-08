57dccf33e9ddae931beaed78cb434e047907aa99
Caleb Carithers <calebcarithers@me.com>
2023-07-08 15:59:49 -0400

    fix server logs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
   experimental: {
