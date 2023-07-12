24fc3a30571ea51c14b6f1b39b102f373396f111
Caleb Carithers <calebcarithers@me.com>
2023-07-12 00:42:35 -0500

    docker

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,4 @@
+import child from "child_process";
 import Koa from "koa";
 import json from "koa-json";
 import logger from "koa-logger";
@@ -7,10 +8,33 @@ import env from "./env";
 const app = new Koa();
 const router = new Router();
 
+const run = (cmd: string) => {
+  return child.execSync(cmd, { stdio: "inherit" });
+};
+
+const createCaseStudy = (castStudy?: any) => {
+  const branchName = `robot/mutual-supply-${Date.now()}`;
+  const dirName = `/tmp/new-study-${Date.now()}`;
+  run(`mkdir ${dirName}`);
+  run(`git clone git@github.com:mutualsupply/site.git ${dirName}/site`);
+  run(
+    `echo "# MUTUAL SUPPLY\n ## MUTUAL SUPPLY\n ### MUTUAL SUPPLY" > ${dirName}/site/src/markdown/mutual-supply.mdx`
+  );
+  run(`cd ${dirName}/site && git status`);
+  run(`cd ${dirName}/site && git branch ${branchName}`);
+  run(`cd ${dirName}/site && git checkout ${branchName}`);
+  run(`cd ${dirName}/site && git add .`);
+  run(`cd ${dirName}/site && git commit -m 'add a test file'`);
+  run(`cd ${dirName}/site && git push origin ${branchName}`);
+  run(`rm -rf ${dirName}`);
+};
+
 router.get("/", async (ctx, next) => {
-  ctx.body = { message: "hello world" };
+  ctx.body = { message: "~~~" };
   await next();
 });
+
+router.get("/case-study", async (ctx, next) => {});
 
 app.use(json());
 app.use(logger());
