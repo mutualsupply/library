6b56b69546a5ac1053e00df9db62ad5f6494185d
caleb <calebcarithers@me.com>
2023-08-19 19:29:05 -0500

    up

--- packages/frontend/src/lib/env.ts
+++ packages/frontend/src/lib/env.ts
@@ -23,13 +23,13 @@ const env: Env = {
   NEXT_PUBLIC_WALLETCONNECT_ID: process.env
     .NEXT_PUBLIC_WALLET_CONNECT_ID as string,
   NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV as AppEnv,
+  NEXT_PUBLIC_SERVER_BASE_URL: process.env
+    .NEXT_PUBLIC_SERVER_BASE_URL as string,
   GITHUB_TOKEN: process.env.GITHUB_TOKEN as string,
   GITHUB_ID: process.env.GITHUB_ID as string,
   GITHUB_SECRET: process.env.GITHUB_SECRET as string,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL as string,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
-  NEXT_PUBLIC_SERVER_BASE_URL: process.env
-    .NEXT_PUBLIC_SERVER_BASE_URL as string,
 }
 
 const frontendKeys = objectKeys(env).filter((key) =>
