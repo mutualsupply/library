4702eed0e2317d7975383464426b55b7911b1e54
caleb <calebcarithers@me.com>
2024-01-02 12:34:07 -0500

    footer z10

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import { cn } from "utils";
 import Image from "next/image";
+import { cn } from "utils";
 import { Badge } from "./ui/badge";
 
 export default function Footer() {
 	return (
-		<div className={cn("flex", "justify-center", "bg-white", "p-4")}>
+		<div className={cn("flex", "justify-center", "bg-white", "p-4", "z-10")}>
 			<div className={cn("flex", "items-center", "gap-1")}>
 				<Image
 					alt="apple-logo"
