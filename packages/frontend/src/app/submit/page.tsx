35213d538029fbc74f70d0dcd0779a79431a5ec5
caleb <calebcarithers@me.com>
2024-01-02 01:25:35 -0500

    fix build

--- packages/frontend/src/app/submit/page.tsx
+++ packages/frontend/src/app/submit/page.tsx
@@ -7,8 +7,8 @@ import { cn, shortenAddress } from "utils";
 import { Button, OPButton } from "../../components/ui/button";
 
 import Image from "next/image";
-import Link from "next/link";
-import { useCallback, useState } from "react";
+import { useState } from "react";
+import { StatusIndicator } from "../../components/StatusIndicator";
 import Github from "../../components/icons/Github";
 import OptimismLogo from "../../components/icons/Optimism";
 import Plus from "../../components/icons/Plus";
@@ -19,12 +19,7 @@ import {
 	AccordionTrigger,
 } from "../../components/ui/accordion";
 import useDrafts from "../../hooks/useDrafts";
-import {
-	GITHUB_OWNER,
-	GITHUB_REPO,
-	NEW_CASE_PAGE_NAME,
-} from "../../lib/constants";
-import { DBCaseStudy } from "../../lib/interfaces";
+import { NEW_CASE_PAGE_NAME } from "../../lib/constants";
 
 const Submit = () => {
 	const { data: session } = useSession();
@@ -217,116 +212,5 @@ const Submit = () => {
 		</div>
 	);
 };
-
-enum Status {
-	Draft = "draft",
-	Submitted = "submitted",
-	Approved = "approved",
-	Rejected = "rejected",
-}
-
-const StatusMap = {
-	[Status.Draft]: {
-		color: "#171712",
-		text: "Draft",
-	},
-	[Status.Submitted]: {
-		color: "#F79009",
-		text: "In review",
-	},
-	[Status.Approved]: {
-		color: "#00A181",
-		text: "Accepted",
-	},
-	[Status.Rejected]: {
-		color: "#EF4444",
-		text: "Rejected",
-	},
-};
-
-interface StatusIndicatorProps {
-	caseStudy: DBCaseStudy;
-	showButton?: boolean;
-}
-
-export function StatusIndicator({
-	caseStudy,
-	showButton = true,
-}: StatusIndicatorProps) {
-	const { submitted, approved } = caseStudy;
-	let status = Status.Approved;
-
-	if (!submitted) {
-		status = Status.Draft;
-	} else if (approved === null) {
-		status = Status.Submitted;
-	} else {
-		status = Status.Rejected;
-	}
-
-	const text = StatusMap[status].text;
-	const background = StatusMap[status].color;
-
-	const renderButton = useCallback((status: Status, draft: DBCaseStudy) => {
-		if (status === Status.Draft) {
-			return (
-				<Link href={`/${NEW_CASE_PAGE_NAME}/${draft.id}`}>
-					<Button
-						size="pill"
-						className={cn(
-							"bg-primary text-white font-aspekta text-xs w-16 py-1",
-						)}
-					>
-						Edit
-					</Button>
-				</Link>
-			);
-		}
-
-		if (status === Status.Submitted) {
-			return (
-				<Link
-					href={`https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/pull/${draft.githubBranchName}`}
-				>
-					<Button
-						size="pill"
-						className={cn(
-							"bg-background text-black border-black border-dashed font-aspekta text-xs w-16 py-1",
-						)}
-					>
-						View
-					</Button>
-				</Link>
-			);
-		}
-
-		if (status === Status.Approved) {
-			return <Link href={`/case/${draft.slug}`}>Edit</Link>;
-		}
-
-		if (status === Status.Rejected) {
-			return <span>😓</span>;
-		}
-	}, []);
-
-	return (
-		<div className={cn("grid grid-cols-2 space-x-1")}>
-			<span className={cn("text-xs flex items-center")}>
-				<div className={cn("inline-flex items-center gap-1")}>
-					<span
-						className={cn("w-2 h-2 rounded-full inline-block")}
-						style={{ background }}
-					/>
-					<span className={cn("text-xs font-aspekta")}>{text}</span>
-				</div>
-			</span>
-			{showButton && (
-				<span className={cn("flex items-center")}>
-					{renderButton(status, caseStudy)}
-				</span>
-			)}
-		</div>
-	);
-}
 
 export default Submit;
